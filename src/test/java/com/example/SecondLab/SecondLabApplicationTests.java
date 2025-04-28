e29a61da72d2de7c1a019847b9808449ae1c4025
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2025-04-28 17:35:46 +0300

    Creating project

---
+++ src/test/java/com/example/SecondLab/SecondLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecondLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
